330cbabef32ca268661cf6833069047d75c926ef
nborja <nelsieborja@gmail.com>
2019-11-19 22:54:19 +0400

    devops: puppeteer test attempt

--- src/storybook.test.ts
+++ src/storybook.test.ts
@@ -1,7 +1,6 @@
 import initStoryshots from '@storybook/addon-storyshots';
-// import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
-// initStoryshots({
-//   test: imageSnapshot({ storybookUrl: 'http://localhost:9009/' }),
-// });
-initStoryshots();
+initStoryshots({
+  test: imageSnapshot({ storybookUrl: 'http://localhost:9009/' }),
+});
